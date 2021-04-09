f91afd66e7832f55df1b645324cb310092496e39
Vibol T <vibolteav@gmail.com>
2021-04-09 12:55:26 +0700

    add more method

--- lib/joker-dmapi/result.rb
+++ lib/joker-dmapi/result.rb
@@ -5,24 +5,28 @@ module JokerDMAPI
     # Get <tt>proc_id</tt>
     # Returned <tt>true</tt> if done (result deleted)
     def complete?(proc_id)
-      response = result_retrieve(proc_id)
-      result = parse_attributes(response[:body].split("\n\n", 1)[0])
+      result = parsed_request(proc_id)
       return false unless result.has_key? :completion_status
+
       case result[:completion_status]
-        when 'ack' then
-          result_delete proc_id
-          true
-        when 'nack' then
-          raise_response response
-        else
-          false
+      when 'ack' then
+        result_delete proc_id
+        true
+      when 'nack' then
+        raise_response response
+      else
+        false
       end
     end
 
     def request_status(proc_id)
+      result = parsed_request(proc_id)
+      result[:completion_status]
+    end
+
+    def parsed_request(proc_id)
       response = result_retrieve(proc_id)
-      result = parse_attributes(response[:body].split("\n\n", 1)[0])
-      result[:completion_status].presence || 'ack'
+      parse_attributes(response[:body].split("\n\n", 1)[0])
     end
 
     def result_retrieve(proc_id)
